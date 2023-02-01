4e1828d67f10c936fbf33664ef6b4101cd5bc685
TlpAyn <talipayan6035@gmail.com>
2023-02-01 11:37:40 +0100

    haftasonuProjeleri

---
+++ src/TEST.java
@@ -0,0 +1,6 @@
+import java.util.Scanner;
+
+public class TEST {
+    public static void main(String[] args) {
+    }
+}
---
+++ src/project_04/Veritabani/Calisanlar.java
@@ -0,0 +1,73 @@
+package project_04.Veritabani;
+import project_04.Modeller.Calisanlar.Calisan;
+import java.util.ArrayList;
+
+public class Calisanlar {
+
+    // Buradaki calisanList static cünkü proje calismaya basladiği anda oluşması lazım. Bunu bir veritabani
+    // gibi görebiliriz. Calisanlarimizin hepsi bu liste icerisinde yer alacak.
+    private static ArrayList<Calisan> calisanList = new ArrayList<>();
+
+    // Calisanlari almak icin basit bir getter method
+    public static ArrayList<Calisan> getCalisanList() {
+        return calisanList;
+    }
+
+    // Bir çalışan eklemek için gerekli method.
+    public static void addACalisan(Calisan calisan) {
+       calisanList.add(calisan);
+    }
+
+    // Bir çalışan silmek için gerekli method.
+    public static void deleteACalisanWithId(String calisanId) {
+
+//  calisanList.removeIf(calisan -> calisan.getCalisanId().equals(calisanId));
+
+
+//        for (Calisan calisan : calisanList) {
+//            if (calisan.getCalisanId().equals(calisanId)) {
+//                calisanList.remove(calisan);
+//            }
+//        }
+
+        for (int i = 0; i < calisanList.size(); i++) {
+            Calisan calisan = calisanList.get(i);
+            if (calisan.getCalisanId().equals(calisanId)) {
+                calisanList.remove(calisan);
+                break;
+            }
+        }
+
+    }
+
+    // Departman kodu verilerek, konsola sadece o departmanda calisanlari yazdirmak için
+    public static void printDepartmandakiCalisanlar(String departmanKodu) {
+
+//        for (Departman departman : Departmanlar.getDepartmanList()) {
+//            if (departman.getDepartmanKodu().equals(departmanKodu)) {
+//                for (Calisan calisan : calisanList) {
+//                    if (calisan.getDepartman().getDepartmanKodu().equals(departmanKodu)) {
+//                        System.out.println(calisan);
+//                    }
+//
+//                }
+//            }
+//        }
+
+        for (Calisan calisan : calisanList) {
+            if (calisan.getDepartman().getDepartmanKodu().equals(departmanKodu)) {
+                System.out.println(calisan);
+            }
+        }
+    }
+
+
+
+    // Calisanlari konsola yazdirmak için
+    public static void printCalisanlar() {
+        for (Calisan calisan : calisanList) {
+            System.out.println(calisan);
+        }
+    }
+
+}
